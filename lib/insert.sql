94ed1dbdbcfce77a9d71adee621478d967928952
Osorabrian <osorabrian@gmail.com>
2023-02-16 20:36:13 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Mr. Chocolate", 39, "M", "blue", "melancholic", "yes");
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Rowdy", 22, "M", "red", "choleric", "no");
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Tabitha", 40, "F", "orange", "phlegmatic", "yes");
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Seargent Brown", 35, "M", "yellow", "sanguine", "no");
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Melissa", 25, "F", "green", "choleric", "yes");
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Grinch", 42, "M", "indigo", "sanguine", "yes");
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Wendy", 20, "F", "purple", "phlegmatic", "no");
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL, 25, "M", "brown", "melancholic", "yes");
